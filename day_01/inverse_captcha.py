b974ce7742420c721aca08f49fe2fb02076dc57d
Meredith Godar <meredith.godar@gmail.com>
2017-12-01 01:47:34 -0600

    Finished Day 1, Part 2

--- day_01/inverse_captcha.py
+++ day_01/inverse_captcha.py
@@ -11,3 +11,18 @@ def inverse_captcha(input_number):
         previous_digit = digit
 
     return sum
+
+
+def halfway_captcha(input_number):
+
+    sum = 0
+
+    sequence = str(input_number)
+    halfway = int(len(sequence)/2)
+    adjusted_sequence = sequence[halfway:] + sequence[:halfway]
+
+    for i in range(0, len(sequence)):
+        if sequence[i] == adjusted_sequence[i]:
+            sum += int(sequence[i])
+
+    return sum
