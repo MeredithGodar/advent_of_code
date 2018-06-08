85e81b6a2e99b6c9ffd41057911717981738c409
Meredith Godar <meredith.godar@gmail.com>
2018-06-08 00:08:57 -0500

    Refactoring

--- day_02/corruption_checksum_test.py
+++ day_02/corruption_checksum_test.py
@@ -5,10 +5,10 @@ import corruption_checksum
 
 class CorruptionChecksumTest(unittest.TestCase):
     def test_corruption_checksum_of_test_input_part1_is_18(self):
-        self.assertEquals(18, corruption_checksum.corruption_checksum('test_input_part1.csv'))
+        self.assertEquals(18, corruption_checksum.corruption_checksum('test_input_part_1.csv'))
 
     def test_evenly_divisible_corruption_checksum_of_test_input_part2_is_18(self):
-        self.assertEquals(9, corruption_checksum.evenly_divisible_corruption_checksum('test_input_part2.csv'))
+        self.assertEquals(9, corruption_checksum.evenly_divisible_corruption_checksum('test_input_part_2.csv'))
 
 
 if __name__ == '__main__':
--- day_04/high_entropy_passphrases_test.py
+++ day_04/high_entropy_passphrases_test.py
@@ -5,10 +5,10 @@ import high_entropy_passphrases
 
 class HighEntropyPassphrasesTest(unittest.TestCase):
     def test_valid_passphrases(self):
-        self.assertEquals(2, high_entropy_passphrases.count_valid_passphrases('test_input_1.txt'))
+        self.assertEquals(2, high_entropy_passphrases.count_valid_passphrases('test_input_part_1.txt'))
 
     def test_valid_passphrases(self):
-        self.assertEquals(3, high_entropy_passphrases.count_valid_passphrases_no_anagrams('test_input_2.txt'))
+        self.assertEquals(3, high_entropy_passphrases.count_valid_passphrases_no_anagrams('test_input_part_2.txt'))
 
 
 if __name__ == '__main__':
