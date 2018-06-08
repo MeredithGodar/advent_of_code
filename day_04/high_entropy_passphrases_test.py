38c9dcd1b80d7643c02a6547d979ee1e5c9be1e2
Meredith Godar <meredith.godar@gmail.com>
2018-06-08 00:12:39 -0500

    Refactoring

--- day_04/high_entropy_passphrases_test.py
+++ day_04/high_entropy_passphrases_test.py
@@ -4,10 +4,10 @@ import high_entropy_passphrases
 
 
 class HighEntropyPassphrasesTest(unittest.TestCase):
-    def test_valid_passphrases(self):
+    def test_valid_passphrases_is_2_for_test_input_part_1(self):
         self.assertEquals(2, high_entropy_passphrases.count_valid_passphrases('test_input_part_1.txt'))
 
-    def test_valid_passphrases_no_anagrams(self):
+    def test_valid_passphrases_no_anagrams_is_3_for_test_input_part_2(self):
         self.assertEquals(3, high_entropy_passphrases.count_valid_passphrases_no_anagrams('test_input_part_2.txt'))
 
 
